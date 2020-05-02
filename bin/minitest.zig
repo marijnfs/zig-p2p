369d876b15ca8f6670ad5777fae01e1dfb75db89
Marijn Stollenga <mstollenga@gmail.com>
2020-05-02 15:40:48 +0200

    remove delim, not needed with dealer - router

--- bin/minitest.zig
+++ bin/minitest.zig
@@ -1,46 +1,49 @@
 const std = @import("std");
+const p2p = @import("p2p");
+const chat = @import("chat");
+
+const chat_functions = @import("chat_functions.zig");
+const router_receiver = chat_functions.router_receiver;
+
+const Socket = p2p.Socket;
+const Message = p2p.Message;
+const work = p2p.work;
+const wi = p2p.work_items;
+
+const Chat = p2p.Chat;
+const cm = p2p.connection_management;
+
+const mem = std.mem;
+const Allocator = mem.Allocator;
+const Buffer = p2p.Buffer;
+
+const functions = p2p.process_functions;
 
 const warn = std.debug.warn;
 const default_allocator = std.heap.page_allocator;
+const c = p2p.c;
 
-pub var zmq_context: *c_void = undefined; //zmq context
-
-pub const c = @cImport({
-    @cInclude("zmq.h");
-    @cInclude("monocypher.h");
-    @cInclude("sys/socket.h");
-    @cInclude("sys/types.h");
-    @cInclude("sys/ioctl.h");
-    @cInclude("netinet/in.h");
-    @cInclude("net/if.h");
-    @cInclude("arpa/inet.h");
-    @cInclude("ifaddrs.h");
-});
+var event_queue: p2p.EventQueue;
 
 pub fn init() !void {
-    zmq_context = c.zmq_ctx_new();
+    p2p.init();
+    chat.init();
 }
 
-pub fn reader() !void {
-    var pull_sock = c.zmq_socket(zmq_context, c.ZMQ_PULL);
-    var router_sock = c.zmq_socket(zmq_context, c.ZMQ_ROUTER);
- 
-    {
-        var str = try std.mem.dupeZ(std.heap.c_allocator, u8, "ipc:///tmp/pull");
-        _ = c.zmq_bind(pull_sock, str);
-    }
-    {
-        var str = try std.mem.dupeZ(std.heap.c_allocator, u8, "ipc:///tmp/router");
-        _ = c.zmq_bind(router_sock, str);
-    }
+pub fn reader(context: void) !void {
+    var pull_sock = try p2p.Socket.init(p2p.connection_management.context, p2p.c.ZMQ_PULL);
+    var router_sock = try p2p.Socket.init(p2p.connection_management.context, p2p.c.ZMQ_ROUTER);
+
+    try pull_sock.bind("ipc:///tmp/pull");
+    try router_sock.bind("ipc:///tmp/router");
 
     while (true) {
         var poll_items: [2]c.zmq_pollitem_t = undefined;
-        poll_items[0].socket = pull_sock;
+        poll_items[0].socket = pull_sock.socket;
         poll_items[0].events = c.ZMQ_POLLIN;
         poll_items[0].revents = 0;
         poll_items[0].fd = 0;
-        poll_items[1].socket = router_sock;
+        poll_items[1].socket = router_sock.socket;
         poll_items[1].events = c.ZMQ_POLLIN;
         poll_items[1].revents = 0;
         poll_items[1].fd = 0;
@@ -52,72 +55,69 @@ pub fn reader() !void {
         }
 
         if (poll_items[0].revents != 0) {
-            warn("item 0\n", .{});
+            warn("got from pull\n", .{});
+
         }
         if (poll_items[1].revents != 0) {
-            warn("item 1\n", .{});
+            warn("got from router\n", .{});
+            var id = try router_sock.recv();
+            var delim = try router_sock.recv();
+            var payload = try router_sock.recv();
+            defer id.deinit();
+            defer delim.deinit();
+            defer payload.deinit();
+
+            
+               
         }
         std.time.sleep(100000000);
     }
 }
 
-pub fn writer() !void {
-    var push_sock = c.zmq_socket(zmq_context, c.ZMQ_PUSH);
-    var str = try std.mem.dupeZ(std.heap.c_allocator, u8, "ipc:///tmp/pull");
-    _ = c.zmq_connect(push_sock, str);
+pub fn writer(context: void) !void {
+    var push_sock = try p2p.Socket.init(p2p.connection_management.context, p2p.c.ZMQ_PUSH);
+    try push_sock.connect("ipc:///tmp/pull");
 
     while (true) {
-        var msg: c.zmq_msg_t = undefined;
-        _ = c.zmq_msg_init(&msg);
-        _ = c.zmq_msg_send(&msg, push_sock, 0);
+        var msg = try p2p.Message.init();
+        try push_sock.send(&msg);
         std.time.sleep(100000000);
     }
 }
 
-pub fn requester() !void {
-    var req_sock = c.zmq_socket(zmq_context, c.ZMQ_REQ);
-    var str = try std.mem.dupeZ(std.heap.c_allocator, u8, "ipc:///tmp/router");
-     _ = c.zmq_connect(req_sock, str);
-
+pub fn requester(context: void) !void {
+    var req_sock = try p2p.Socket.init(p2p.connection_management.context, p2p.c.ZMQ_REQ);
+    try req_sock.connect("ipc:///tmp/router");
 
     while (true) {
         warn("read socket\n", .{});
-        var msg: c.zmq_msg_t = undefined;
-
-        _ = c.zmq_msg_init(&msg);
-        defer _ = c.zmq_msg_close(&msg);
-        _ = c.zmq_msg_send(&msg, req_sock, 0);
-
-        var msg_recv: c.zmq_msg_t = undefined;
-        _ = c.zmq_msg_init(&msg_recv);
-        defer _ = c.zmq_msg_close(&msg_recv);
-        _ = c.zmq_msg_recv(&msg_recv, req_sock, 0);
+        var blanc = try Message.init();
+        try req_sock.send(&blanc);
+        var msg = try req_sock.recv();
 
         std.time.sleep(100000000);
     }
 }
 
-pub fn reader_void(v: void) void {
-    reader() catch unreachable;
+pub fn reader_void(context: void) void {
+    reader(context) catch unreachable;
 }
 
-pub fn writer_void(v: void) void {
-    writer() catch unreachable;
+pub fn writer_void(context: void) void {
+    writer(context) catch unreachable;
 }
 
-pub fn requester_void(v: void) void {
-    requester() catch unreachable;
+pub fn requester_void(context: void) void {
+    requester(context) catch unreachable;
 }
 
 pub fn main() anyerror!void {
     warn("Chat\n", .{});
     try init();
 
-    var t = try std.Thread.spawn({}, reader_void);
-    var t2 = try std.Thread.spawn({}, writer_void);
-    var t3 = try std.Thread.spawn({}, requester_void);
+    _ = try p2p.thread_pool.add_thread({}, reader_void);
+    _ = try p2p.thread_pool.add_thread({}, writer_void);
+    _ = try p2p.thread_pool.add_thread({}, requester_void);
 
-    t.wait();
-    t2.wait();
-    t3.wait();
+    p2p.thread_pool.join();
 }
